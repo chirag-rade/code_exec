36312527ee63e7e0a06a130ae7e3d012decf6cec
Alexei Vasilkov <yohannewgen@gmail.com>
2024-06-04 23:54:34 +0400

    [feat,fix]: add arg to provide parsed notebook to the agent instead of path, fix missing subprocess import, black formatting on 2 files

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -1,33 +1,29 @@
-
 from dotenv import load_dotenv
-from rich.markdown import Markdown
 from rich import print as md
+from rich.markdown import Markdown
+
 load_dotenv()
-from langchain_core.prompts import ChatPromptTemplate
-from langgraph.graph import END, StateGraph
 import json
-from langchain_core.messages import (
-    FunctionMessage,
-    HumanMessage,
-)
+
+from langchain_core.messages import FunctionMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.graph import END, StateGraph
 from langgraph.prebuilt.tool_executor import ToolExecutor, ToolInvocation
 from Models.models import LLMModel
-from .tools import *
-from .schemas import *
-from .utils import *
+
 from .agents import *
 from .prompts import *
+from .schemas import *
+from .tools import *
+from .utils import *
 
 
 def tool_node(state):
-
     """This runs tools in the graph
 
     It takes in an agent action and calls that tool and returns the result."""
     messages = state["messages"]
-  
+
     # Based on the continue condition
     # we know the last message involves a function call
     last_message = messages[-1]
@@ -37,7 +33,9 @@ def tool_node(state):
             last_message.additional_kwargs["function_call"]["arguments"]
         )
     except:
-        tool_input = {"code":last_message.additional_kwargs["function_call"]["arguments"]} #sometimes the actual code is sent as a string instead of {code:"code"}
+        tool_input = {
+            "code": last_message.additional_kwargs["function_call"]["arguments"]
+        }  # sometimes the actual code is sent as a string instead of {code:"code"}
     # We can pass single-arg inputs by value
     if len(tool_input) == 1 and "__arg1" in tool_input:
         tool_input = next(iter(tool_input.values()))
@@ -54,13 +52,15 @@ def tool_node(state):
     )
     # We return a list, because this will get added to the existing list
     return {"messages": [function_message]}
-        
+
+
 def code_extractor_node(state):
-    x =  HumanMessage(json.dumps(code_extractor(state)))
+    x = HumanMessage(json.dumps(code_extractor(state)))
     return {
         "messages": [x],
         "sender": "code_extractor",
     }
+
 
 def happy_pather_node(state):
     x = HumanMessage(json.dumps(happy_pather(state)))
@@ -69,6 +69,7 @@ def happy_pather_node(state):
         "sender": "happy_pather",
     }
 
+
 def edge_caser_node(state):
     x = HumanMessage(json.dumps(edge_caser(state)))
     return {
@@ -76,42 +77,51 @@ def edge_caser_node(state):
         "sender": "edge_caser",
     }
 
+
 def issue_finder_node(state):
     x = HumanMessage(json.dumps(issue_finder(state)))
     return {
         "messages": [x],
         "sender": "issue_finder",
     }
- 
+
+
 def combine_tests_node(state):
     import copy
+
     testers_input = copy.deepcopy(state["messages"])
     happy_output = happy_pather(testers_input)
     edge_output = edge_caser(testers_input)
     issues = issue_finder(testers_input)
-    combined_output = {"happy":happy_output['happy_paths'], "edge":edge_output['edge_cases'], "issues":issues['issues'], "code":happy_output["code"]}
+    combined_output = {
+        "happy": happy_output["happy_paths"],
+        "edge": edge_output["edge_cases"],
+        "issues": issues["issues"],
+        "code": happy_output["code"],
+    }
     return {
         "messages": [HumanMessage(json.dumps(combined_output))],
         "sender": "testers",
     }
-    
+
+
 def testers_graph(notebook):
     workflow1 = StateGraph(AgentState)
     workflow1.add_node("code_extractor", code_extractor_node)
-    workflow1.add_node("testers",combine_tests_node)
+    workflow1.add_node("testers", combine_tests_node)
     workflow1.add_edge("code_extractor", "testers")
     workflow1.add_edge("testers", END)
     workflow1.set_entry_point("code_extractor")
     graph = workflow1.compile()
-    
-    
-    messages_in = messages_in = [HumanMessage(content="Here is the conversation {}".format(notebook))]
-    input_message = {
-    "chat_history": [],
-    "messages": messages_in,
-    "user_config": {},
-    }
 
+    messages_in = messages_in = [
+        HumanMessage(content="Here is the conversation {}".format(notebook))
+    ]
+    input_message = {
+        "chat_history": [],
+        "messages": messages_in,
+        "user_config": {},
+    }
 
     for s in graph.stream(input_message, {"recursion_limit": 20}):
         print("AGENT:", s)
@@ -119,14 +129,20 @@ def testers_graph(notebook):
         content = s[agent]["messages"][-1].content
         if agent == "testers":
             h = json.loads(content)
-            tests_content = f"Happy: {h['happy']}\n\nEdge: {h['edge']}\n\nIssues: {h['issues']}"
+            tests_content = (
+                f"Happy: {h['happy']}\n\nEdge: {h['edge']}\n\nIssues: {h['issues']}"
+            )
             tests_content = Markdown(tests_content)
             md(tests_content)
         elif agent != "tool_node":
-            #check if it is trying to call a function/tool
+            # check if it is trying to call a function/tool
             if "function_call" in s[agent]["messages"][-1].additional_kwargs:
-                function_being_called = s[agent]["messages"][-1].additional_kwargs['function_call']['name']
-                args = s[agent]["messages"][-1].additional_kwargs['function_call']['arguments']
+                function_being_called = s[agent]["messages"][-1].additional_kwargs[
+                    "function_call"
+                ]["name"]
+                args = s[agent]["messages"][-1].additional_kwargs["function_call"][
+                    "arguments"
+                ]
                 content = f"I am calling the function `{function_being_called}` with the following arguments: {args}"
                 content = Markdown(content)
                 md(content)
@@ -140,31 +156,38 @@ def testers_graph(notebook):
         else:
             content = Markdown(content)
             md(content)
-            
-            
+
     return h
 
+
 def code_runner_graph(test, code, recursion_limit):
-    
+
     workflow2 = StateGraph(AgentState)
     workflow2.add_node("code_runner", code_runner)
-    workflow2.add_node("tool_node",  tool_node)
+    workflow2.add_node("tool_node", tool_node)
     workflow2.add_conditional_edges(
-            "code_runner",
-            router,
-            { "END": END, "tool_node": "tool_node"},
-        )
+        "code_runner",
+        router,
+        {"END": END, "tool_node": "tool_node"},
+    )
     workflow2.add_conditional_edges(
-            "tool_node",
-            lambda x: x["sender"],
-            {"code_runner":"code_runner"},
-        )
+        "tool_node",
+        lambda x: x["sender"],
+        {"code_runner": "code_runner"},
+    )
 
-    workflow2.set_entry_point("code_runner",)
+    workflow2.set_entry_point(
+        "code_runner",
+    )
     graph2 = workflow2.compile()
 
-    messages_in = [HumanMessage(content=
-                                "HERE IS THE CODE:\n\n ```{code}``` \n\n  INSTRUCTIONS:\n\n Write the test code to test for this \n {test}".format(code = code, test =test))]
+    messages_in = [
+        HumanMessage(
+            content="HERE IS THE CODE:\n\n ```{code}``` \n\n  INSTRUCTIONS:\n\n Write the test code to test for this \n {test}".format(
+                code=code, test=test
+            )
+        )
+    ]
 
     input_message = {
         "chat_history": [],
@@ -172,16 +195,19 @@ def code_runner_graph(test, code, recursion_limit):
         "user_config": {},
     }
 
-
     for s in graph2.stream(input_message, {"recursion_limit": recursion_limit}):
         print("AGENT:", s)
         agent = list(s.keys())[0]
         content = s[agent]["messages"][-1].content
         if agent != "tool_node":
-            #check if it is trying to call a function/tool
+            # check if it is trying to call a function/tool
             if "function_call" in s[agent]["messages"][-1].additional_kwargs:
-                function_being_called = s[agent]["messages"][-1].additional_kwargs['function_call']['name']
-                args = s[agent]["messages"][-1].additional_kwargs['function_call']['arguments']
+                function_being_called = s[agent]["messages"][-1].additional_kwargs[
+                    "function_call"
+                ]["name"]
+                args = s[agent]["messages"][-1].additional_kwargs["function_call"][
+                    "arguments"
+                ]
                 content = f"I am calling the function `{function_being_called}` with the following arguments: {args}"
                 content = Markdown(content)
                 md(content)
@@ -195,12 +221,22 @@ def code_runner_graph(test, code, recursion_limit):
         else:
             content = Markdown(content)
             md(content)
-            
+
     return s[agent]["messages"][-1].content
 
-def run(notebook_path, happy_cases_n = 2, edge_cases_n = 2, recursion_limit=30):
-  
-    notebook = load_notebook(notebook_path)
+
+def run(
+    notebook_path,
+    happy_cases_n=2,
+    edge_cases_n=2,
+    recursion_limit=30,
+    loaded_notebook=None,
+):
+
+    if loaded_notebook:
+        notebook = loaded_notebook
+    else:
+        notebook = load_notebook(notebook_path)
     test_cases = testers_graph(notebook)
 
     happy_paths = test_cases["happy"]
@@ -209,23 +245,16 @@ def run(notebook_path, happy_cases_n = 2, edge_cases_n = 2, recursion_limit=30):
     code = test_cases["code"]
     final_results = []
 
-
-    for i,t  in enumerate(focus):
-        final_results.append(code_runner_graph(t, code, recursion_limit=recursion_limit))
-
-
+    for i, t in enumerate(focus):
+        final_results.append(
+            code_runner_graph(t, code, recursion_limit=recursion_limit)
+        )
 
     chat_template = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "{main_prompt}"
-            ),
-            MessagesPlaceholder(variable_name="messages")
-        ])
+        [("system", "{main_prompt}"), MessagesPlaceholder(variable_name="messages")]
+    )
 
-    chat_template=chat_template.partial(main_prompt=main_prompt)
-
+    chat_template = chat_template.partial(main_prompt=main_prompt)
 
     single_message = HumanMessage(
         f"Here is the actual Conversation: \n {notebook} \n"
@@ -237,20 +266,15 @@ def run(notebook_path, happy_cases_n = 2, edge_cases_n = 2, recursion_limit=30):
         "Your Evaluation is for both the code provided and other aspect of the conversation"
     )
 
-
-    evaluator= LLMModel(
+    evaluator = LLMModel(
         provider="openai_api",
         model="gpt-4o",
         use_tool=True,
         use_history=False,
         output_schema=NotebookWiseFeedback,
-        prompt_template=chat_template  
+        prompt_template=chat_template,
     )
 
     ans = evaluator([single_message])
 
-
     return ans
-
-
-
--- Agents/tools.py
+++ Agents/tools.py
@@ -1,8 +1,11 @@
-from langchain_core.tools import tool
-import os
-import io
 import contextlib
+import io
+import os
+import subprocess
 from typing import Annotated
+
+from langchain_core.tools import tool
+
 
 @tool
 def python_repl(code: Annotated[str, "The python code to execute."]):
@@ -31,68 +34,74 @@ def python_repl(code: Annotated[str, "The python code to execute."]):
         return f"Successfully executed:\n```python\n{code}\n```\nStdout: {result}"
 
 
-
 @tool
-def write_file(name: Annotated[str, "The name of the file."], content: Annotated[str, "The content of the file."]):
+def write_file(
+    name: Annotated[str, "The name of the file."],
+    content: Annotated[str, "The content of the file."],
+):
     """This tool writes a file with the given name and content to the Playground directory."""
     # Define the directory
     directory = "Playground"
-    
+
     # Create the directory if it doesn't exist
     if not os.path.exists(directory):
         os.makedirs(directory)
-    
+
     # Define the file path
     file_path = os.path.join(directory, name)
-    
+
     # Write the content to the file
     try:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write(content)
         status = "File created successfully"
     except Exception as e:
         status = f"Failed to create file. Error: {repr(e)}"
-    
+
     # Return the name and status of creation
     return {"full_path": file_path, "status": status}
+
 
 @tool
 def create_directory(name: Annotated[str, "The name of the directory."]):
     """This tool creates a directory with the given name in the Playground directory."""
     # Define the parent directory
     parent_directory = "Playground"
-    
+
     # Create the parent directory if it doesn't exist
     if not os.path.exists(parent_directory):
         os.makedirs(parent_directory)
-    
+
     # Define the directory path
     directory_path = os.path.join(parent_directory, name)
-    
+
     # Create the directory
     try:
         os.makedirs(directory_path)
         status = "Directory created successfully"
     except Exception as e:
         status = f"Failed to create directory. Error: {repr(e)}"
-    
+
     # Return the name and status of creation
     return {"full_path": directory_path, "status": status}
 
 
 @tool
 def run_js_code(js_code: Annotated[str, "The JavaScript code to run."]):
-    
     """This tool runs the provided JavaScript code and returns the stdout."""
     try:
-        result = subprocess.run(["node", "-e", js_code], capture_output=True, text=True, check=True)
-        return {"result": result.stdout, "status": "JavaScript code executed successfully"}
+        result = subprocess.run(
+            ["node", "-e", js_code], capture_output=True, text=True, check=True
+        )
+        return {
+            "result": result.stdout,
+            "status": "JavaScript code executed successfully",
+        }
     except subprocess.CalledProcessError as e:
-        return {"result": None, "status": f"Failed to execute JavaScript code. Error: {repr(e)}"}
+        return {
+            "result": None,
+            "status": f"Failed to execute JavaScript code. Error: {repr(e)}",
+        }
 
-all_tools = [
-            python_repl,
-            write_file,
-            run_js_code,
-            create_directory
-        ]
+
+all_tools = [python_repl, write_file, run_js_code, create_directory]
